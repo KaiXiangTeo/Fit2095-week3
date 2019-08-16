7a420ac5d2d080364945a75554495eafd3117d69
Teo Kai Xiang <dennis_teo@dyn-118-138-59-205.its.monash.edu.au>
2019-08-16 15:11:52 +1000

    server.js with two operation

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+/////////<<<<<<<<<<!!!!!!express.js!!!!!!!>>>>>>>>>>>//////////
 //able to have multiple get
 
 let express = require('express'); //third party package,built in with node.js !!!>>>>>>find node module to get express package
@@ -13,19 +15,32 @@ app.get("/",function(req,res){
 
 app.get("/wiki/:para/fit2095/:id",function(req,res){   //must match wiki/anything**/fit2095/id e.g. http://localhost:8080/wiki/Melbo/fit2095/23
     console.log(req.url);
-    console.log(req.params);
+    console.log(req.params);  //for path localhost:8080/wiki/melbourne
 }); //any word after wiki is a parameter (except number)
 
-app.get("/about",function(req,res){   //each function do one task, no need to worried about other 
-    res.send("This is an about page");
+app.get("/report",function(req,res){
+    console.log(req.url);
+    console.log(req.query); //for query localhost:8080/wiki/?name=Tim&age=24
+    console.log("The name is ==",req.query.name);
+    console.log("The age is ==",req.query.age);
+    
 });
 
+// app.get("/about",function(req,res){   //each function do one task, no need to worried about other 
+//     res.send("This is an about page");
+// });
 
+app.get('/about', function (req, res) { 
+    res.send('about')
+});
+
+app.get('/random.text', function (req, res) {
+    res.send('random.text')
+  })
 
 app.get("/contactUs",function(req,res){
     res.send("This is the Contact Us page");
 })
-
 
 
 app.post("/calc",function(req,res){
@@ -36,6 +51,10 @@ app.listen(8080);
 
 
 
+
+
+
+//////////////////<<<<node.js>>>>/////////////////////////
 // let http=require('http');
 
 // http.createServer(function (req,res) {
